640f9462038f66c1097b311ee93a99408b81f4bb
lahds13 <sliverhyweldaar@gmail.com>
2021-09-10 20:31:36 +0300

    Cleaned up code.

--- app/src/main/java/lahds/relyme/UserInterface/EmojiView/Search/EmojiSearchView.java
+++ app/src/main/java/lahds/relyme/UserInterface/EmojiView/Search/EmojiSearchView.java
@@ -1,21 +1,3 @@
-/*
- * Copyright (C) 2020 - Amir Hossein Aghajari
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-
 package lahds.relyme.UserInterface.EmojiView.Search;
 
 import android.annotation.SuppressLint;
@@ -75,7 +57,7 @@ public class EmojiSearchView extends FrameLayout implements SearchViewInterface 
 
     public EmojiSearchView(Context context, EmojiBase emojiView) {
         super(context);
-        if (!EmojiManager.isAXEmojiView(emojiView))
+        if (!EmojiManager.isEmojiView(emojiView))
             throw new RuntimeException("EmojiView must be an instance of EmojiView or SingleEmojiView.");
         this.theme = EmojiManager.getEmojiViewTheme();
         this.base = emojiView;
